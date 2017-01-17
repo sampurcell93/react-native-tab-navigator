32f0e35ea30f06aaee0ac5ec717651079015e071
Samuel Purcell <samuel.purcell@tufts.edu>
2017-01-16 20:04:32 -0500

    Disable swipe behavior of tab bar on android for perf reasons

--- TabBar.js
+++ TabBar.js
@@ -55,94 +55,93 @@ export default class TabBar extends React.Component {
     }
   }
   componentWillMount() {
-    this.pan = PanResponder.create({
-      // Ask to be the responder:
-      onStartShouldSetPanResponder: (evt, gestureState) => false,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => false,
-      onMoveShouldSetPanResponder: (evt, gestureState) => {
-        if (this.state.overrideSwipe && this.state.isOpen) {
-          return false;
-        } else if (this.state.isOpen && gestureState.dy < 0) {
-          return false;
-        } else if (!this.state.isOpen && gestureState.dy > 0) {
-          return false;
-        } else if (Math.abs(gestureState.dx / gestureState.dy) >= 1.5) {
-          return false;
-        } else if (gestureState.dy === 0) {
-          return false;
-        }
-        return true;
-      },
-      onMoveShouldSetPanResponderCapture: (evt, gestureState) => false,
+    if (Platform.OS === 'android') {
+      this.pan = PanResponder.create({});
+    } else {
+      this.pan = PanResponder.create({
+        // Ask to be the responder:
+        onStartShouldSetPanResponder: (evt, gestureState) => false,
+        onStartShouldSetPanResponderCapture: (evt, gestureState) => false,
+        onMoveShouldSetPanResponder: (evt, gestureState) => {
+          if (this.state.overrideSwipe && this.state.isOpen) {
+            return false;
+          } else if (this.state.isOpen && gestureState.dy < 0) {
+            return false;
+          } else if (!this.state.isOpen && gestureState.dy > 0) {
+            return false;
+          } else if (Math.abs(gestureState.dx / gestureState.dy) >= 1.5) {
+            return false;
+          } else if (gestureState.dy === 0) {
+            return false;
+          }
+          return true;
+        },
+        onMoveShouldSetPanResponderCapture: (evt, gestureState) => false,
 
-      onPanResponderGrant: (evt, gestureState) => {
-        // The guesture has started. Show visual feedback so the user knows
-        // what is happening!
+        onPanResponderGrant: (evt, gestureState) => {
+          // The guesture has started. Show visual feedback so the user knows
+          // what is happening!
 
-        // gestureState.{x,y}0 will be set to zero now
-      },
-      onPanResponderMove: (evt, gestureState) => {
-        const {dy} = gestureState;
-        const {maxHeight} = this.state;
-        // The most recent move distance is gestureState.move{X,Y}
-        if (!this.state.isOpen && this.props.canSwipeUp) {
-          if (dy <= 0) {
-            this.state.positionY.setValue(dy)
-            this.state.tabBarPosition.setValue(45 * Math.abs(dy / maxHeight))
-            this.state.tabBarWrapperOpacity.setValue(1 - (dy / maxHeight))
-            this.state.swipeUpContentOpacity.setValue(.25 + Math.abs(dy / maxHeight))
-          }
-        } else {
-          if (dy >= 0) {
-            this.state.positionY.setValue(maxHeight + dy)
-            this.state.tabBarPosition.setValue(45 - 45 * Math.abs(dy / maxHeight))
-            this.state.tabBarWrapperOpacity.setValue(dy / Math.abs(maxHeight))
-            this.state.swipeUpContentOpacity.setValue(1 - (dy / Math.abs(maxHeight)))
-          }
-        }
-        // }
-        // The accumulated gesture distance since becoming responder is
-        // gestureState.d{x,y}
-      },
-      onPanResponderTerminationRequest: (evt, gestureState) => true,
-      onPanResponderRelease: (evt, gestureState) => {
-        // The user has released all touches while this view is the
-        const vy = Math.abs(gestureState.vy);
-        if (!this.state.isOpen) {
-          if (Math.abs(gestureState.dy * (1 - vy)) <= height / 10) {
-            this.animateClosed();
+          // gestureState.{x,y}0 will be set to zero now
+        },
+        onPanResponderMove: (evt, gestureState) => {
+          const {dy} = gestureState;
+          const {maxHeight} = this.state;
+          // The most recent move distance is gestureState.move{X,Y}
+          if (!this.state.isOpen && this.props.canSwipeUp) {
+            if (dy <= 0) {
+              this.state.positionY.setValue(dy)
+              this.state.tabBarPosition.setValue(45 * Math.abs(dy / maxHeight))
+              this.state.tabBarWrapperOpacity.setValue(1 - (dy / maxHeight))
+              this.state.swipeUpContentOpacity.setValue(.25 + Math.abs(dy / maxHeight))
+            }
           } else {
-            this.animateOpen()
+            if (dy >= 0) {
+              this.state.positionY.setValue(maxHeight + dy)
+              this.state.tabBarPosition.setValue(45 - 45 * Math.abs(dy / maxHeight))
+              this.state.tabBarWrapperOpacity.setValue(dy / Math.abs(maxHeight))
+              this.state.swipeUpContentOpacity.setValue(1 - (dy / Math.abs(maxHeight)))
+            }
           }
-        } else {
-          if (gestureState.dy >= 0 && Math.abs(gestureState.dy * (1 + vy)) >= height / 7) {
-            this.animateClosed()
+          // }
+          // The accumulated gesture distance since becoming responder is
+          // gestureState.d{x,y}
+        },
+        onPanResponderTerminationRequest: (evt, gestureState) => true,
+        onPanResponderRelease: (evt, gestureState) => {
+          // The user has released all touches while this view is the
+          const vy = Math.abs(gestureState.vy);
+          if (!this.state.isOpen) {
+            if (Math.abs(gestureState.dy * (1 - vy)) <= height / 10) {
+              this.animateClosed();
+            } else {
+              this.animateOpen()
+            }
           } else {
+            if (gestureState.dy >= 0 && Math.abs(gestureState.dy * (1 + vy)) >= height / 7) {
+              this.animateClosed()
+            } else {
+              this.animateOpen();
+            }
+          }
+          // responder. This typically means a gesture has succeeded
+        },
+        onPanResponderTerminate: (evt, gestureState) => {
+          // Another component has become the responder, so this gesture
+          // should be cancelled
+          if (this.isOpen) {
             this.animateOpen();
+          } else {
+            this.animateClosed();
           }
-        }
-        // responder. This typically means a gesture has succeeded
-      },
-      onPanResponderTerminate: (evt, gestureState) => {
-        // Another component has become the responder, so this gesture
-        // should be cancelled
-        if (this.isOpen) {
-          this.animateOpen();
-        } else {
-          this.animateClosed();
-        }
-        return true;
-      },
-      onShouldBlockNativeResponder: (evt, gestureState) => {
-        // Returns whether this component should block native components from becoming the JS
-        // responder. Returns true by default. Is currently only supported on android.
-        return false;
-      },
-    });
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (!prevProps.canSwipeUp && this.props.canSwipeUp && this.swipeUpContent) {
-      this.swipeUpContent.forceUpdate();
+          return true;
+        },
+        onShouldBlockNativeResponder: (evt, gestureState) => {
+          // Returns whether this component should block native components from becoming the JS
+          // responder. Returns true by default. Is currently only supported on android.
+          return false;
+        },
+      });
     }
   }
   animateOpen() {
